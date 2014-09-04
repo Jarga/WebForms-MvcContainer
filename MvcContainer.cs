fd6a7463344a397e9cafd8adfb432cf7309122cb
Sean McAdams <sean.j.mcadams@gmail>
2014-09-04 14:06:34 -0400

    Added Container

---
+++ MvcContainer.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MarketOnce.Web.Common
+{
+    public class MvcContainer
+    {
+        private Controller _controller;
+        private ViewPage _viewPage;
+
+        public class WebFormsController : Controller { }
+
+        public HtmlHelper Html { get; private set; }
+        public UrlHelper Url { get; private set; }
+        public dynamic ViewBag { get; private set; }
+
+        public MvcContainer(HttpContext context)
+        {
+            this._controller = new WebFormsController();
+            this._viewPage = new ViewPage();
+            InitializeHtmlHelper(context);
+        }
+
+        private void InitializeHtmlHelper(HttpContext context)
+        {
+            var httpContext = new HttpContextWrapper(context);
+            var controllerContext = new ControllerContext(httpContext, new RouteData(), _controller);
+            var viewContext = new ViewContext(controllerContext, new WebFormView(controllerContext, "Views"), new ViewDataDictionary(), new TempDataDictionary(), TextWriter.Null);
+            Html = new HtmlHelper(viewContext, _viewPage);
+            Url = new UrlHelper(new RequestContext(httpContext, RouteTable.Routes.GetRouteData(httpContext) ?? new RouteData()));
+            ViewBag = viewContext.ViewBag;
+        }
+
+        public void Dispose()
+        {
+            _controller.Dispose();
+            _viewPage.Dispose();
+        }
+    }
+}
